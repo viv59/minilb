437f000d63021e8cfa0ff3c20d630fab342a71ea
viv59 <210010059@iitdh.ac.in>
2026-07-02 01:40:45 +0530

    added readme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,14 @@ function App() {
 
   return (
     <>
-      Hello vivek
+      <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        miniLB
+      </h1>
+      <p>
+        Hello world!
+      </p>
+    </div>
     </>
   )
 }
